6cb33137c4207f86805ac471d1bd698f75b4b5bd
Jeff Bredenkamp <jeffbredenkamp@gmail.com>
2023-01-11 21:01:26 +0000

    dark toggle added

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,36 @@ document.body.onload = function(){
       }
     }, 1000)
   }
+  
+  const toggleBtn = document.getElementById("toggle-btn");
+  const theme = document.getElementById("theme");
+  let darkMode = localStorage.getItem("dark-mode");
+  
+  const enableDarkMode = () => {
+    theme.classList.add("dark-mode");
+    toggleBtn.classList.remove("dark-mode-toggle");
+    localStorage.setItem("dark-mode", "enabled");
+    toggleBtn.classList.add("dark-active");
+  };
+  
+  const disableDarkMode = () => {
+    theme.classList.remove("dark-mode");
+    toggleBtn.classList.add("dark-mode-toggle");
+    localStorage.setItem("dark-mode", "disabled");
+    toggleBtn.classList.remove("dark-active");
+  };
+  
+  if (darkMode === "enabled") {
+    enableDarkMode(); // set state of darkMode on page load
+  }
+  
+  toggleBtn.addEventListener("click", (e) => {
+    darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
+    if (darkMode === "disabled") {
+      enableDarkMode();
+      toggleBtn.classList.add("dark-active");
+    } else {
+      disableDarkMode();
+      toggleBtn.classList.remove("dark-active");
+    }
+  });
